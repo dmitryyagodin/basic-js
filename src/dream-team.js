1dc5f784c723184ff275b970dd38067ca05f3005
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2021-03-15 23:55:45 +0200

    feat: solve count-cats and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(team) {
+  if (Array.isArray(team)) {
+    return team.
+      map(item => typeof item === 'string' ? item.trim()[0].toUpperCase() : '').
+      sort((a,b) => a > b ? 1 : -1).
+      join('');
+  } else {
+    return false
+  }
+  
 };
+
+// console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max'])); //` => `'ADMM'`
+// console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null])); // => `'LOO'`
+// console.log(createDreamTeam([
+//   ['David Abram'],
+//   ['Robin Attfield'],
+//   'Thomas Berry',
+//   ['Paul R.Ehrlich'],
+//   'donna Haraway',
+//   ' BrIaN_gOodWiN  ',
+//   {
+//     0: 'Serenella Iovino'
+//   },
+//   'Erazim Kohak',
+//   '  val_plumwood',
+// ]));
